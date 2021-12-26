67ce4bea1088550164360c4bb7f48b11f97ca7dd
tami5 <kkharji@protonmail.com>
2021-12-26 06:21:42 +0300

    feat(*): read devenv configuration to local state

--- src/tami5/devenv.clj
+++ src/tami5/devenv.clj
@@ -1,4 +1,6 @@
-(ns tami5.devenv)
+(ns tami5.devenv
+  (:require [clojure.test :refer [are run-tests]]
+            [clojure.java.io :as io]))
 
 (def ^:private state
   (atom
@@ -8,7 +10,7 @@
     :integrant/with-duct? false
 
     :watch/active? false
-    :watch/dirs nil
+    :watch/paths nil
     :watch/handle nil
     :watch/pattern #"[^.].*(\.clj|\.edn)$"
     :watch/timestamp "[hh:mm:ss]"
@@ -19,11 +21,26 @@
     :env/local-clj? false
     :env/started? false}))
 
+(def ^:private merge-state! (partial swap! state merge))
+
 (defn init
   "Process user options and prepare dev environment:
   if no configuration is given, then defaults will be used."
-  ([])
-  ([config]))
+  {:test
+   #(are [lhs rhs] [lhs (some? (get (init rhs) (first lhs)))]
+      [:dev/integrant? true] {:integrant/file-path "..."}
+      [:dev/duct? true] {:integrant/with-duct? true :integrant/file-path "...."}
+      [:dev/duct? false] {:integrant/with-duct? true}
+      [:integrant/prep true] {:integrant/prep (fn [] (println "prep integrant"))}
+      [:watch/paths true] nil)}
+  ([] (init nil))
+  ([config]
+   (let [s (merge-state! config)]
+     (merge-state!
+      {:dev/integrant? (some? (s :file-path))
+       :dev/duct? (and (s :with-duct?) (s :file-path))
+       :dev/local-clj? (io/resource "local.clj")
+       :watch/paths #(or (:watch/paths s) (:env/paths s))}))))
 
 (defn start
   "Start development environment:
