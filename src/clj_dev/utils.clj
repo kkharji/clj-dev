a8f1cc35a4f5084bdb6eb62bbf79e2defcb18d35
tami5 <kkharji@protonmail.com>
2021-12-28 18:22:44 +0300

    enh(util): optional log without timestamp

--- src/clj_dev/utils.clj
+++ src/clj_dev/utils.clj
@@ -3,7 +3,8 @@
             [clojure.java.io :as io]
             [duct.core :as duct]
             [clojure.tools.namespace.repl :as repl]
-            [integrant.repl :as igr]))
+            [integrant.repl :as igr]
+            [clojure.pprint :as pprint]))
 
 (defn- get-timestamp []
   (some-> :watch-timestamp
@@ -11,13 +12,18 @@
           (java.text.SimpleDateFormat.)
           (.format (java.util.Date.))))
 
-(defn log [key]
-  (when key
-    (println (str (get-timestamp) " ===============================\n" (keyword "environment" (name key))))))
+(defn log
+  ([key] (log key true))
+  ([key with-timestamp?]
+   (let [leading (when with-timestamp?
+                   (str (get-timestamp)
+                        " ===============================\n"))]
+     (when key
+       (println (str leading (keyword "environment" (name key))))))))
 
 (defn set-integrant-prep! []
   (igr/set-prep!
-   #(let [resource (io/resource (s/config :integrant-file-path))]
+   #(if-let [resource (println (io/resource (s/config :integrant-file-path)))]
       (if-not s/duct?
         (slurp resource)
         (-> (duct/read-config resource)
@@ -29,3 +35,19 @@
 
 (def set-refresh-dirs
   (partial apply repl/set-refresh-dirs))
+
+(defmacro spy
+  "A simpler version of Timbre's spy.
+  credit: https://github.com/thiru
+  Returns the eval'd expression."
+  [expr]
+  `(let [evaled# ~expr]
+     (print (str '~expr " => "))
+     (pprint/pprint evaled#)
+     evaled#))
+
+(defn- logger [ns code used]
+  (println (str (keyword ns (name code)) " Got: " (pr-str used))))
+
+(def debug (partial logger "debug"))
+(def error (partial logger "error"))
